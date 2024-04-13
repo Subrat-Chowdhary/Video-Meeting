73497d40f1a6b1dd6e9dee64aa14bb50791ca200
Subrat Chowdhary <subratchowdhary@outlook.com>
2024-04-14 02:25:09 +0530

    Fixes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 rounded-full">
+        <div className="flex h-full flex-col justify-between max-md:px-12 max-md:py-12 lg:p-11 rounded-full md:p-16">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base text-yellow-500 font-extrabold">
             Development Environment..
           </h2>
