a38fad9414c1ae309492daa57028c100ae9813ac
Subrat Chowdhary <subratchowdhary@outlook.com>
2024-04-13 19:14:00 +0530

    Join Meeting Button css

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -75,7 +75,7 @@ const MeetingSetup = ({
         <DeviceSettings />
       </div>
       <Button
-        className="rounded-md bg-green-500 px-4 py-2.5"
+        className="rounded-md bg-gradient-to-r from-purple-950 to-purple-500 text-white font-bold rounded-3xl transition duration-300 p-3 px-8 px-4 py-2.5"
         onClick={() => {
           call.join();
 
